186f74e982f1f1af52adf8601cae403de344cce9
Shark <Shark85423@gmail.com>
2017-05-01 17:13:32 +0800

    更新Asset_model， Asset_Controller

--- app/Asset.php
+++ app/Asset.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Asset extends Model
 {
-    //
+    //資產
+    protected $table = 'asset_form';
+    protected $fillable = [
+        'id',
+        'name',
+        'category',
+        'date',
+        'cost',
+        'staues',
+        'keeper',
+        'lendable',
+        'location',
+        'remark',
+        'vendor',
+        'warranty',
+    ];
 }
--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -9,4 +9,42 @@ use App\Http\Requests;
 class AssetController extends Controller
 {
     //
+    public function index()
+    {
+        $asset=Asset::orderBy('created_at', 'DESC')->get();
+        $data=['asset'=>$asset];
+        return view('admin.assets.index', $data);
+    }
+
+    public function create()
+    {
+        return view('admin.assets.create');
+    }
+
+    public function edit($id)
+    {
+        $asset=Asset::find($id);
+        $data = ['asset' => $asset];
+
+        return view('admin.assets.edit', $data);
+    }
+    public function update(PostRequest $request, $id)
+    {
+        $asset=Asset::find($id);
+        $asset->update($request->all());
+
+        return redirect()->route('admin.assets.index');
+    }
+    public function store(PostRequest $request)
+    {
+        Asset::create($request->all());
+        return redirect()->route('admin.assets.index');
+    }
+    public function destroy($id)
+    {
+        Asset::destroy($id);
+
+        return redirect()->route('admin.assets.index');
+    }
+
 }
