6bfad3b3d110ec944dc1d078d7f101bdff0ed944
Shark <Shark85423@gmail.com>
2017-05-01 18:53:35 +0800

    routes建立asset.index/create/store

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,18 @@ Route::get('posts/{id}', ['as' => 'posts.show' , 'uses' => 'PostsController@show
 // 後台
 Route::group(['prefix' => 'admin'], function() {
     Route::get('/', ['as' => 'admin.dashboard.index', 'uses' => 'AdminDashboardController@index']);
-
+    //
     Route::get('posts'          , ['as' => 'admin.posts.index' , 'uses' => 'AdminPostsController@index']);
     Route::get('posts/create'   , ['as' => 'admin.posts.create' , 'uses' => 'AdminPostsController@create']);
     Route::get('posts/{id}/edit', ['as' => 'admin.posts.edit'   , 'uses' => 'AdminPostsController@edit']);
     Route::patch('posts/{id}'   , ['as' => 'admin.posts.update' , 'uses' => 'AdminPostsController@update']);
     Route::post('posts'         , ['as' => 'admin.posts.store'  , 'uses' => 'AdminPostsController@store']);
     Route::delete('posts/{id}'  , ['as' => 'admin.posts.destroy', 'uses' => 'AdminPostsController@destroy']);
+
+    //資產
+    Route::get('assets'          , ['as' => 'assets.index' , 'uses' => 'AssetController@index']);
+    Route::get('assets/create'   , ['as' => 'assets.create' , 'uses' => 'AssetController@create']);
+    Route::post('assets'         , ['as' => 'asset.store'  , 'uses' => 'AssetController@store']);
 });
 
 Route::get('/tracy',function(){throw new \Exception('Tracy works');} );
