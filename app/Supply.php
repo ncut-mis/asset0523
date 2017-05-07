0bf0b4996e008cd8b131b03dbe8ca673e9a49473
S2092752\SSU-CHIEH <baby0925862059@gmail.com>
2017-05-08 00:23:38 +0800

    耗材路徑更改 複製supplies views

---
+++ app/Supply.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Supply extends Model
+{
+    //
+    protected $table = 'supplies';
+    protected $fillable = [
+
+    ];
+}
