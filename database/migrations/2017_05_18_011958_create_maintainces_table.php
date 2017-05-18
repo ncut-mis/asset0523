a13e226232fa5a84daf53954bb0a1a1bd3b122b9
Shark <Shark85423@gmail.com>
2017-05-18 09:43:43 +0800

    建立maintainces_db

---
+++ database/migrations/2017_05_18_011958_create_maintainces_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMaintaincesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maintainces', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->integer('a_id')->index();
+            $table->integer('v_id')->index();
+            $table->integer('mitems_id');
+            $table->date('date');
+            $table->string('status',20);
+            $table->string('method',20);
+            $table->text('remark')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('maintainces');
+    }
+}
