b5d0c1e575c312d18ee52c14b2175127f6916f54
S2092752\SSU-CHIEH <baby0925862059@gmail.com>
2017-05-19 15:44:01 +0800

    領取model

--- app/Receive.php
+++ app/Receive.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Receive extends Model
 {
     //
+    protected $table = 'receives';
+    protected $fillable = [
+        'u_id',
+'s_id',
+'datatime'.
+'quantity',
+    ];
+    public function user()
+{
+    return $this->hasMany(User::class);
 }
+    public function supply()
+    {
+        return $this->hasMany(Supply::class);
+    }
+}
+
+
