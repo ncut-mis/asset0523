645bd018480d0e5402aa3ce19fc8e02d9782457a
Shark <Shark85423@gmail.com>
2017-07-17 17:10:08 +0800

    刪除posts範例

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,6 @@ Route::get('posts/{id}', ['as' => 'posts.show' , 'uses' => 'PostsController@show
 Route::group(['prefix' => 'admin'], function() {
     Route::get('/', ['as' => 'admin.dashboard.index', 'uses' => 'AdminDashboardController@index']);
     //　
-    Route::get('posts', ['as' => 'admin.posts.index', 'uses' => 'AdminPostsController@index']);
-    Route::get('posts/create', ['as' => 'admin.posts.create', 'uses' => 'AdminPostsController@create']);
-    Route::get('posts/{id}/edit', ['as' => 'admin.posts.edit', 'uses' => 'AdminPostsController@edit']);
-    Route::patch('posts/{id}', ['as' => 'admin.posts.update', 'uses' => 'AdminPostsController@update']);
-    Route::post('posts', ['as' => 'admin.posts.store', 'uses' => 'AdminPostsController@store']);
-    Route::delete('posts/{id}', ['as' => 'admin.posts.destroy', 'uses' => 'AdminPostsController@destroy']);
 
     //資產
     Route::get('assets', ['as' => 'admin.assets.index', 'uses' => 'AssetController@index']);              //資產主畫面
