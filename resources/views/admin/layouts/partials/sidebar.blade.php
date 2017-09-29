abee04fac4c995b68714e67a77a43f18f5e50c2d
shark <shark85423@gmail.com>
2017-09-29 15:15:28 +0800

    QR-2

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -8,7 +8,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="{{ route('admin.dashboard.index') }}">盈錫報修系統</a>
+        <a class="navbar-brand" href="{{ route('admin.dashboard.index') }}">盈錫資訊設備報修系統</a>
     </div>
     <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
